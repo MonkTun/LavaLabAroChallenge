f51939066ac47549f8f085f8c7eb20a0cbd01314
Tundraswan <75334356+MonkTun@users.noreply.github.com>
2024-09-13 17:43:06 -0700

    webupdate5

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,4 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/{LavaLabAroChallenge}", plugins: [react()], }) 
+export default defineConfig({ base: "/LavaLabAroChallenge/", plugins: [react()], }) 
